e6f388575e8bcc33b9d5f33aec45bf68f88e2a38
Raphael Azeredo <rgazeredo@gmail.com>
2023-05-15 11:33:13 -0300

    reset password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        Informe seu endereço de e-mail e enviaremos um link de redefinição de senha por e-mail que permitirá que você
+        escolha uma nova.
     </div>
 
     <!-- Session Status -->
@@ -11,14 +12,15 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-label for="email" value="E-mail" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                Recuperar senha
             </x-primary-button>
         </div>
     </form>
